0497b5fd9f70d622194568d99266d7bc8edf9962
dracken24 <dracken24@gmail.com>
2023-01-03 21:18:36 -0500

    first

---
+++ microshell.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dracken24 <dracken24@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 21:11:14 by dracken24         #+#    #+#             */
+/*   Updated: 2023/01/03 21:16:50 by dracken24        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv, char **env)
+{
+    int i = 0;
+    char **token;
+    
+    if (argc > 1)
+    {
+        
+    }
+}
