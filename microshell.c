2213b52605af5d7e47d1c6f8106d9b5761235bc2
nadia <dracken24@gmail.com>
2023-01-10 17:11:27 -0500

    ok

--- microshell.c
+++ microshell.c
@@ -6,7 +6,7 @@
 /*   By: nadesjar <dracken24@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 21:11:14 by dracken24         #+#    #+#             */
-/*   Updated: 2023/01/06 13:59:59 by nadesjar         ###   ########.fr       */
+/*   Updated: 2023/01/10 17:08:42 by nadesjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-int		ft_count_cmds(char **argv)
+int ft_count_cmds(char **argv)
 {
 	int i = 0;
-	int	ret = 0;
+	int ret = 0;
 
 	while (argv[i])
 	{
@@ -32,11 +31,14 @@ int		ft_count_cmds(char **argv)
 	return (ret + 1);
 }
 
-int	ft_end_with(int argc, char **argv)
+int	ft_end_with_pipe(char **argv, int i)
 {
-	if (argv[1][0] == ';' || argv[argc -1][0] == ';'
-		|| argv[1][0] == '|' || argv[argc -1][0] == '|')
-		return (-1);
+	while (argv[i] && argv[i][0] != '|')
+	{
+		i++;
+	}
+	if (argv[i] && argv[i][0] == '|')
+		return (i);
 	return (0);
 }
 
@@ -45,14 +47,14 @@ int main(int argc, char **argv, char **env)
 	// (void)env;
 	int		nbr_cmds = 0;
 	int     i = 1;
-	// int		k = 1;
-	// int     fd[2];
+	int		k = 1;
+	int     fd[2];
 	pid_t   pid;
 	
 	if (argc > 1)
 	{
 		nbr_cmds = ft_count_cmds(argv);
-		printf("NBR CMDS: %d\n", nbr_cmds);
+		// printf("NBR CMDS: %d\n", nbr_cmds);
 		while (nbr_cmds > 0 && argv[i])
 		{
 			// argv++;
@@ -64,13 +66,30 @@ int main(int argc, char **argv, char **env)
 			}
 			if (pid == 0)
 			{
-				// argv[0] = NULL;
-				// printf("exec\n");
-				if (execve(argv[1], &argv[1], env) == -1)
+				k = ft_end_with_pipe(argv, i);
+				printf("K: %d\n", k);
+				if ( k > 0)
 				{
-					printf("EXECBAD\n");
+					k += i;
+					printf("K: %d\n", k);
+					// i = k;
+					argv[k] = NULL;
+
+					if (pipe(fd) < 0)
+					{
+						printf("BAD PIPE\n");
+						exit(-1);
+					}
+					dup2(fd[1], 0);
+					close(fd[0]);
+					
+				}
+				if (execve(argv[i], &argv[i], env) == -1)
+				{
+					// printf("EXECBAD\n");
 					exit(1);
 				}
+				i = k + 1;
 				// printf("exec2\n");
 			}
 			if (waitpid(pid, NULL, 0) == -1)
@@ -78,8 +97,14 @@ int main(int argc, char **argv, char **env)
 				write(2, "Error, waitpid\n", 15);
 				exit(0);
 			}
-			printf("GOOD\n");
+			if (ft_end_with_pipe(argv, i) > 0)
+			{
+				dup2(fd[0], 1);
+				close(fd[1]);
+			}
+			// printf("GOOD\n");
 			// }
+			// while ()
 			i++;
 			nbr_cmds--;
 		}
