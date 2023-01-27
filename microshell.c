ec97b72fbe1d4dda7cd5372eee973a60a7915d66
nadia <dracken24@gmail.com>
2023-01-27 13:02:43 -0500

    work

--- microshell.c
+++ microshell.c
@@ -1,11 +1,9 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/param.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 
-int countMDS(char **argv)
+int countCmds(char **argv)
 {
 	int ret = 0;
 
@@ -18,132 +16,117 @@ int countMDS(char **argv)
 	return (ret + 1);
 }
 
+void putStr(char *str, int quit, int fd)
+{
+	for (int i = 0; str[i]; i++)
+		write(fd, &str[i], 1);
+	if (quit == -1)
+		exit(-1);
+}
+
 int endWith(char **argv, int i, char good, char bad)
 {
-	int k = 0;
-
-	for (; argv[i]; i++)
+	for (int k = i; argv[k]; k++)
 	{
-		if (argv[i][0] == good)
-			return (k);
-		else if (argv[i][0] == bad)
+		if (argv[k][0] == good)
+			return (k - i);
+		else if (argv[k][0] == bad)
 			return (0);
-		k++;
 	}
 
 	return (0);
 }
 
-void    putStr(char *str, int fd, int quit)
-{
-	for (int i = 0; str[i]; i++)
-		write(fd, &str[i], 1);
-	if (quit != 0)
-		exit(quit);
-}
-
-void    cd(char *path)
-{
-	if (chdir(path) == -1)
-	{
-		putStr("error: cd: cannot change directory to ", 2, 0);
-		putStr(path, 2, 0);
-		putStr("\n", 2, 0);
-	}
-}
-
-// void	print_cmds(char **argv, int i, int endWp, int endWsc, int nbr_cmds)
-// {
-// 	printf("--------------------------------------------------------\n");
-// 	printf("I: %d\n\n", i);
-// 	for (; argv[i]; i++)
-// 		printf("%s\n", argv[i]);
-// 	printf("\n");
-// 	printf("cmd: %d\n", nbr_cmds);
-// 	printf("EndWp: %d\n", endWp);
-// 	printf("EndWsc: %d\n", endWsc);
-// 	printf("---------   -------------   ------------------   -------\n");
-// }
-
-void	execute(char **argv, char **env, int endWpipe, int i, int pid, int *fd)
+void execCmds(char **argv, char **env, int endWP, int i, int *fd, pid_t pid)
 {
 	if (pid == 0)
 	{
-		if (endWpipe > 0)
+		if (endWP > 0)
 		{
 			close(fd[0]);
-			dup2(fd[1], STDOUT_FILENO);
+			dup2(fd[1], 1);
 		}
 		if (execve(argv[i], &argv[i], env))
 		{
-			putStr("error: cannot execute ", 2, 0);
-			putStr(argv[i], 2, 0);
-			putStr("\n", 2, 0);
+			putStr("error: cannot execute ", 0, 2);
+			putStr(argv[i], 0, 2);
+			putStr("\n", -1, 2);
 		}
 	}
-	else if (endWpipe > 0)
+	else
 	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
+		if (endWP > 0)
+		{
+			close(fd[1]);
+			dup2(fd[0], 0);
+		}
+		if (waitpid(pid, NULL, 0) == -1)
+			putStr("error, waitpid fail\n", -1, 2);
 	}
-	waitpid(pid, NULL, 0);
-		// putStr("error: fatal\n", 2, -1);
+}
+
+void cd(char *path)
+{
+	if (chdir(path) < 0)
+	{
+		putStr("error: cd: cannot change directory to ", 0, 2);
+		putStr(path, 0, 2);
+		putStr("\n", 0, 2);
+	}
 }
 
 int main(int argc, char **argv, char **env)
 {
-	int		i = 0;
-	int		ct = 1;
-	int		fd[2];
-	int		endWsc = 0;
-	int		endWpipe = 0;
-	int		nbr_cmds = 0;
-	pid_t	pid;
+	(void)env;
+	pid_t pid;
+	int i = 0;
+	int ct = 1;
+	int fd[2];
+	int endWP = 0;
+	int endWsc = 0;
+	int nbrCmds = 1;
 
 	if (argc > 1)
 	{
+		nbrCmds = countCmds(argv);
 		argv++;
-		nbr_cmds = countMDS(argv);
-		for (; nbr_cmds > 0; nbr_cmds--)
+		for (; nbrCmds > 0 && argv[i]; nbrCmds--)
 		{
-			if(argv[i] && argv[i][0] == ';')
+			if (argv[i] && argv[i][0] == ';')
 				i++;
 			else
 			{
-				endWpipe = endWith(argv, i, '|', ';');
-				if (endWpipe == 0)
+				endWP = endWith(argv, i, '|', ';');
+				if (!endWP)
 					endWsc = endWith(argv, i, ';', '|');
-				if (endWpipe || endWsc)
-					argv[i + endWpipe + endWsc] = NULL;
-				// print_cmds(argv, i, endWpipe, endWsc, nbr_cmds);
-				if (ct == 1 && strncmp(argv[i], "cd", 2) == 0)
+				if (endWsc || endWP)
+					argv[i + endWsc + endWP] = NULL;
+
+				if (strncmp(argv[i], "cd", 3) == 0 && ct == 1)
 				{
 					if (!argv[i + 1] || argv[i + 1][0] == ';')
-						putStr("error: cd: bad arguments\n", 2, 0);
+						putStr("error: cd: bad arguments\n", 0, 2);
 					else
-						cd(argv[i]);
+						cd(argv[i + 1]);
 					ct = 0;
 				}
-
-				if (pipe(fd) == -1)
-					putStr("error: fatal\n", 2, -1);
-				if ((pid = fork()) == -1)
-					putStr("error: fatal\n", 2, -1);
-
-				execute(argv, env, endWpipe, i, pid, fd);
-
-				if (endWpipe)
-					ct = 0;
-				i += endWpipe + endWsc + 1;
+				else
+				{
+					if (pipe(fd) == -1)
+						putStr("error, pipe fail\n", -1, 2);
+					if ((pid = fork()) == -1)
+						putStr("error, fork fail\n", -1, 2);
+					execCmds(argv, env, endWP, i, fd, pid);
+				}
+				i += endWP + endWsc + 1;
 			}
-			if (!argv[i])
-				break ;
-			endWpipe = 0;
+			if (endWP)
+				ct = 0;
+
+			endWP = 0;
 			endWsc = 0;
 			close(fd[0]);
 			close(fd[1]);
 		}
 	}
-
-	return (0);
 }
